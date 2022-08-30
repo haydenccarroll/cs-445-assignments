a57b5fda866c047873309cab0a4c1690d14921e9
Hayden Carroll <haydencarroll77@gmail.com>
2022-08-29 17:14:13 -0700

    fix: added a lil more code

--- assignment_1/scanType.h
+++ assignment_1/scanType.h
@@ -1,13 +1,12 @@
 #ifndef SCANTYPE_H
 #define SCANTYPE_H
 
-
 struct TokenData {
-    int tokenclass; // token class
-    int linenum; // line where found
+    int  tokenclass; // token class
+    int  linenum; // line where found
     char *tokenstr; // what string was actually read
     char cvalue; // any character value
-    int nvalue; // any numeric value or Boolean value
+    int  nvalue; // any numeric value or Boolean value
     char *svalue; // any string value e.g. an id
 };
 
